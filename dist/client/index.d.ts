408a7d5ab81115e0c8ef5c982611fa9bb647fc05
ytl003 <tielongyang@163.com>
2023-09-08 23:24:44 +0800

    feat: rm dist

--- dist/client/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { Plugin } from '@nocobase/client';
-export declare class DumuWangEditorFieldPlugin extends Plugin {
-    afterAdd(): Promise<void>;
-    beforeLoad(): Promise<void>;
-    load(): Promise<void>;
-}
-export default DumuWangEditorFieldPlugin;
